4b5c29bcd34d5055fb824a0fbb370d10d6ef4fa7
Fleury Anthony <anth.fleury@gmail.com>
2018-03-07 20:28:37 +0100

    Added heading to scrapping

--- StreetviewScrapper/scrapper.py
+++ StreetviewScrapper/scrapper.py
@@ -8,36 +8,40 @@ file = open('api_key.txt', 'r')
 api_key = file.read()
 
 # Define parameters for street view api
-apiargs = {
+params = [{
   'size': '640x640', # max 640x640 pixels
   'location': '',
   'pitch': '0',
   'fov' : '90',
-  'heading' : '180',
+  'heading' : '',
   'key': api_key
-}
+}]
 
 # Parse GPX file
-tree = etree.parse('gpx_data/delporren.gpx')
+tree = etree.parse('gpx_data/delporren2.gpx')
 
-namespaces = {'ns':'http://www.topografix.com/GPX/1/1'}
+namespaces = {'ns':'http://www.topografix.com/GPX/1/0'}
 
 # Iterate over all locations in the gpx file
-for index, location_node in enumerate(tree.xpath("//ns:trk/ns:trkseg/ns:trkpt", namespaces=namespaces), start=1):
-    #locations[index] = {'lat':location_node.get("lat"), 'lon':location_node.get("lon")}
-
+for index, location_node in enumerate(tree.xpath('//ns:trk/ns:trkseg/ns:trkpt', namespaces=namespaces), start=1):
+    children = location_node.getchildren()
     print('-----', index, '-----')
+    if children :
+        print('heading', children[0].text)
     print('latitude', location_node.get("lat"))
     print('longitude', location_node.get("lon"))
 
-    # Add the position of this location to apiargs
-    apiargs['location'] += f';{location_node.get("lat")},{location_node.get("lon")}'
+    # Set the position of this location to params
+    params[0]['location'] = f'{location_node.get("lat")},{location_node.get("lon")}'
+    # Set heading of this position to params
+    if children :
+        params[0]['heading'] = f'{children[0].text}'
 
-# Get a list of all possible queries from multiple parameters
-api_list = google_streetview.helpers.api_list(apiargs)
+    # Create a results object
+    results = google_streetview.api.results(params)
 
-# Create a results object for all possible queries
-results = google_streetview.api.results(api_list)
+    # Download images to directory 'maps'
+    results.download_links('maps')
 
-# Download images to directory 'downloads'
-results.download_links('maps')
+    # Rename file
+    os.rename('maps/gsv_0.jpg', f'maps/gsv_{index}.jpg')
