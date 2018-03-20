4c114ef8213a85edfa080735a5fbc5b3338d324c
Fleury Anthony <anth.fleury@gmail.com>
2018-03-20 23:33:34 +0100

    Modified a bit speed algorithm
    - Image switch directly after input now
    - Switching between K and L keys is reset when speed is going down.
    - Using now get_time() from pygame to get elapsed time

--- MetadriveGame/metadrive.py
+++ MetadriveGame/metadrive.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 
 from constants import *
 
+from pyaudio import PyAudio
+
 def map_range_to_range(min_a, max_a, min_b, max_b, value_a):
     ratio = value_a / (max_a - min_a)
     value_b = ratio * (max_b - min_b) + min_b
@@ -12,8 +14,6 @@ pygame.init()
 
 # Events
 pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP])
-
-
 
 # Icon
 # icone = pygame.image.load(icon_image)
@@ -39,7 +39,6 @@ images_count = 631
 
 # Time
 travel_time = 180
-last_time = time.time()
 total_time = 0
 delta_time = 1
 time_since_last_image = 0
@@ -56,7 +55,7 @@ min_image_speed = 0
 max_image_speed = math.ceil(average_image_speed*2)
 current_image_speed = min_image_speed
 last_image_speed = min_image_speed
-image_speed_deceleration = 1
+image_speed_deceleration = 2
 
 print('Average image speed : ', average_image_speed)
 print('Max image speed : ', max_image_speed)
@@ -66,9 +65,6 @@ while 1:
     # Clock tick : set the MAXIMUM FPS
     clock.tick(tick_rate)
 
-    current_view = pygame.image.load(f'maps/gsv_{index_view}.jpg')
-    current_view_rect = current_view.get_rect()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
@@ -76,19 +72,20 @@ while 1:
             if event.key == K_k and btn_right_pressed == True:
                 btn_left_pressed = True
                 btn_right_pressed = False
-
                 key_pressed_count += 1
+                # Mode one by one when key speed is not evaluated yet
+                if not current_image_speed:
+                    index_view += 1
 
             if event.key == K_l and btn_left_pressed == True:
                 btn_left_pressed = False
                 btn_right_pressed = True
-
                 key_pressed_count += 1
-
+                # Mode one by one when key speed is not evaluated yet
+                if not current_image_speed:
+                    index_view += 1
     # Time Calc
-    current_time = time.time()
-    elapsed = current_time - last_time
-    last_time = current_time
+    elapsed = clock.get_time()/1000
     total_time += elapsed
     time_since_last_image += elapsed
 
@@ -99,17 +96,21 @@ while 1:
 
     # Calculating speeds
     if total_time > delta_time:
+
         current_key_speed = key_pressed_count/total_time # is this really necessary ? ~ 16 ms
         if current_key_speed > max_key_speed:
             current_key_speed = max_key_speed
 
         temp_image_speed = map_range_to_range(min_key_speed, max_key_speed, min_image_speed, max_image_speed, current_key_speed)
-
+        
         # Deceleration
         if temp_image_speed < current_image_speed:
             current_image_speed -= image_speed_deceleration
             if current_image_speed < 0:
                 current_image_speed = 0
+            # Reset foot order when speed is going down
+            btn_left_pressed = True
+            btn_right_pressed = True
         else:
             current_image_speed = temp_image_speed
 
@@ -119,8 +120,11 @@ while 1:
         print('===== ', current_key_speed, ' =====')
         print('##### ', current_image_speed, ' #####')
 
+    # Load image
+    current_view = pygame.image.load(f'maps/gsv_{index_view}.jpg')
+    current_view_rect = current_view.get_rect()
 
-
+    # Draw image
     screen.fill(black)
     screen.blit(current_view, current_view_rect)
     pygame.display.flip()
